8ce6cbacc76af22b1e463e05c3dcea06a375a6ae
DanielRojasF <danielrf98@gmail.com>
2022-11-11 07:26:53 -0600

    Commit FireBase,MainActivity,Gradle

--- app/src/main/java/com/farmatica/MainActivity.kt
+++ app/src/main/java/com/farmatica/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.farmatica
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,15 +11,11 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 
-
 class MainActivity : AppCompatActivity() {
-
     //Definimos un objeto para acceder a la autenticacion de firebase
     private lateinit var auth: FirebaseAuth
-
     //Definimos un objeto para acceder a los elemento de la pantalla xml
     private lateinit var binding: IniciarSesionBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = IniciarSesionBinding.inflate(layoutInflater)
@@ -28,22 +23,18 @@ class MainActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)
         auth = Firebase.auth
         setContentView(R.layout.iniciar_sesion)
-
         //Se define el metodo para hacer el login
         binding.btIniciarSesion.setOnClickListener {
             haceLogin()
         }
-
         //Se define el metodo para hacer el registro de usuario
         binding.btRegistrarse.setOnClickListener {
             haceRegistro()
         }
     }
-
     private fun haceRegistro() {
         val email = binding.etEmail.text.toString()
         val clave = binding.etPassword.text.toString()
-
         //Se hace el registro
         auth.createUserWithEmailAndPassword(email, clave)
             .addOnCompleteListener(this) { task ->
@@ -58,11 +49,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
-
     private fun haceLogin() {
         val email = binding.etEmail.text.toString()
         val clave = binding.etPassword.text.toString()
-
         //Se hace el registro
         auth.signInWithEmailAndPassword(email, clave)
             .addOnCompleteListener(this) { task ->
@@ -77,14 +66,12 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
-
     private fun actualiza(user: FirebaseUser?) {
         if (user != null) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
-
     //Esto hara que una vez autenticado.... no pida mas autenticarse a menos que se cierre la sesion
     public override fun onStart() {
         super.onStart()
