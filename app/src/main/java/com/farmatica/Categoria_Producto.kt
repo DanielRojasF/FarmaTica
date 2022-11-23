d5dacad59f64eca19ebb90610a59f2f8f4d9d7da
Dani1783 <danielaceciliajcr@gmail.com>
2022-11-23 16:55:17 -0600

    Commit Actualización layouts

---
+++ app/src/main/java/com/farmatica/Categoria_Producto.kt
@@ -0,0 +1,11 @@
+package com.farmatica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Categoria_Producto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categoria_producto)
+    }
+}
