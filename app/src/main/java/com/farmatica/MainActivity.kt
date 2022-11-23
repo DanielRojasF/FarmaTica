990676e4678835ee13070efcd8915f178189ae9f
DanielRojasF <danielrf98@gmail.com>
2022-11-22 18:37:08 -0600

    Commit Iniciar_sesion arreglada

--- app/src/main/java/com/farmatica/MainActivity.kt
+++ app/src/main/java/com/farmatica/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.farmatica.databinding.IniciarSesionBinding
+import com.farmatica.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -15,22 +15,23 @@ class MainActivity : AppCompatActivity() {
     //Definimos un objeto para acceder a la autenticacion de firebase
     private lateinit var auth: FirebaseAuth
     //Definimos un objeto para acceder a los elemento de la pantalla xml
-    private lateinit var binding: IniciarSesionBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = IniciarSesionBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
         FirebaseApp.initializeApp(this)
         auth = Firebase.auth
-        setContentView(R.layout.iniciar_sesion)
+
+
         //Se define el metodo para hacer el login
-        binding.btIniciarSesion.setOnClickListener {
-            haceLogin()
-        }
+        binding.btIniciarSesion.setOnClickListener { haceLogin() }
         //Se define el metodo para hacer el registro de usuario
-        binding.btRegistrarse.setOnClickListener {
-            haceRegistro()
-        }
+        binding.btRegistrarse.setOnClickListener { haceRegistro() }
+
+
     }
     private fun haceRegistro() {
         val email = binding.etEmail.text.toString()
@@ -68,7 +69,7 @@ class MainActivity : AppCompatActivity() {
     }
     private fun actualiza(user: FirebaseUser?) {
         if (user != null) {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, Menu_Principal::class.java)
             startActivity(intent)
         }
     }
