2a77a6b378795fe83090079b62e68f8ce193e69d
DanielRojasF <danielrf98@gmail.com>
2022-12-12 21:47:57 -0600

    Commmit SignInWithGoogle

--- app/src/main/java/com/farmatica/MainActivity.kt
+++ app/src/main/java/com/farmatica/MainActivity.kt
@@ -6,13 +6,21 @@ import android.util.Log
 import android.view.KeyEvent
 import android.widget.Toast
 import com.farmatica.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     //Definimos un objeto para acceder a la autenticacion de firebase
     private lateinit var auth: FirebaseAuth
     //Definimos un objeto para acceder a los elemento de la pantalla xml
@@ -32,8 +40,51 @@ class MainActivity : AppCompatActivity() {
         //Se define el metodo para hacer el registro de usuario
         binding.btRegistrarse.setOnClickListener { haceRegistro() }
 
+        //Google sign in Option
+        val gso
+        = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id_r))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        binding.btGoogle.setOnClickListener { googleSignIn() }
+
+
 
     }
+
+    private fun googleSignIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,5000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 5000){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val cuenta = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(cuenta.idToken!!)
+            } catch (e: ApiException){
+
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){task->
+                if (task.isSuccessful){
+                    val user = auth.currentUser
+                    actualiza(user)
+                }else{
+                    actualiza(null)
+                }
+            }
+    }
+
     private fun haceRegistro() {
         val email = binding.etEmail.text.toString()
         val clave = binding.etPassword.text.toString()
@@ -75,11 +126,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
     //Esto hara que una vez autenticado.... no pida mas autenticarse a menos que se cierre la sesion
-    public override fun onStart() {
-        super.onStart()
-        val usuario = auth.currentUser
-        actualiza(usuario)
-    }
+    //public override fun onStart() {
+    //    super.onStart()
+    //    val usuario = auth.currentUser
+    //    actualiza(usuario)
+    //}
 
 
 }
